76b58b6093c4c1da792a00726d44ea6fba7939c9
christian.velasco <christian.velasco@zone43-ec.cs.ucalgary.ca>
2018-11-30 12:48:54 -0700

    setting up basic stuff

---
+++ 526-as4/pfilter.py
@@ -0,0 +1,25 @@
+import sys
+import base64
+
+def main():
+
+    if len(sys.argv) != 3:
+        print("Wrong number of args")
+        return -1
+
+    # Storing names of files
+    f_rules = sys.argv[1]
+    f_rawpacker = sys.argv[2]
+
+
+    # Open RawPacket File
+
+
+
+    with open( f_rawpacker ,)
+
+
+
+
+
+main()
